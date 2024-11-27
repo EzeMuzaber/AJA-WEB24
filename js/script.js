b4882a95f88975ad26d74b7cadcf92f41cce9388
ezequiel muzaber <ezequielmuzaber@gmail.com>
2024-11-26 23:42:32 -0300

    primera carga inicial

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Formulario de contacto enviado con éxito.');
+});
+
+document.getElementById('cvForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('CV enviado correctamente.');
+});
